a87567b53bf112702b05bd0cd8c96400007c8067
WIV-01 <info@wilsoniv.nl>
2025-05-16 11:31:10 +0200

    app.js

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ let contract;
 
 const CONTRACT_ADDRESS = "0x9ddd5962f9441a0400be0ab95777381bbfd4ec59"; // ✅ Your deployed contract
 const USDC_ADDRESS = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"; // ✅ USDC Mainnet
+const CONTRACT_ABI = window.CONTRACT_ABI; // ✅ Contract ABI
 
 // Minimal ABI for interacting with USDC's approve and allowance
 const usdcAbi = [
