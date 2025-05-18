d9a34fe31884f9747d48771f0d1107039c6a2126
WIV-01 <info@wilsoniv.nl>
2025-05-18 23:34:42 +0200

    app.js

--- js/app.js
+++ js/app.js
@@ -132,10 +132,12 @@ function dh_trades(price) {
     }
 
     // Calculate average price from arr_buy_Trades, or use current price if empty
-    const avg = arr_buy_Trades.length === 0
+    /*const avg = arr_buy_Trades.length === 0
       ? price
-      : arr_buy_Trades.reduce((sum, trade) => sum + Number(trade["ETH Price (USD)"]), 0) / arr_buy_Trades.length;
-
+      : arr_buy_Trades.reduce((sum, trade) => sum + Number(trade["ETH Price (USD)"]), 0) / arr_buy_Trades.length;*/
+    const tempSum = arr_buy_Trades.reduce((sum, trade) => sum + Number(trade["ETH Price (USD)"]), 0) + price;
+    const avg = tempSum / (arr_buy_Trades.length + 1);
+    
     // Add trade to table
     arr_buy_Trades.push({
       Timestamp: new Date().toLocaleString(),
