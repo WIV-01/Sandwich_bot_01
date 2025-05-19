c57382d69924f7ff774eb333702b2bb5ab0d332a
WIV-01 <info@wilsoniv.nl>
2025-05-19 17:42:53 +0200

    app.js

--- js/app.js
+++ js/app.js
@@ -185,7 +185,7 @@ function dh_trades(price) {
         "Average": avg.toFixed(2),
         "MG factor": Math.pow(dbl_Martingale_factor, arr_buy_Trades.length)
       });
-    } esle {
+    } else {
       return;
     }
     
