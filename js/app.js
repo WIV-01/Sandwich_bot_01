5f86d300aa9fc60ff9a20845ade2a268eba3910e
WIV-01 <info@wilsoniv.nl>
2025-05-20 21:09:35 +0200

    app.js

--- js/app.js
+++ js/app.js
@@ -316,6 +316,8 @@ async function updateBalances(address) {
     
     document.getElementById("ethBalance").innerText = `ETH: ${parseFloat(ethFormatted).toFixed(4)} ETH`;
     document.getElementById("usdcBalance").innerText = `USDC: ${parseFloat(usdcFormatted).toFixed(2)} USDC`;
+    document.getElementById("contract_EthBalance").innerText = `ETH: ${parseFloat(contractEthBalance).toFixed(4)} ETH`;
+    document.getElementById("contract_usdcBalance").innerText = `USDC: ${parseFloat(usdValue).toFixed(2)} USDC`;
   } catch (err) {
 
       //Console message
