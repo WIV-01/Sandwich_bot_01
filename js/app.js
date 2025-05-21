8563bdaeee25465540da9ffd7a356609480ba337
WIV-01 <info@wilsoniv.nl>
2025-05-21 14:10:37 +0200

    app.js

--- js/app.js
+++ js/app.js
@@ -171,8 +171,7 @@ function dh_trades(price) {
   const _colname_Action = "Action";
   const dbl_Entryprice_temp = 0;
   const dbl_Entryprice = 0;
-    
-  String str_Action = "-";
+  const str_Action = "-";
   
   try {
     // === Validate price ===
